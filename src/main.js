25c02cd177fa3942cc838d7a1d1898b4985da969
Talai <tsa@tut.by>
2026-02-03 09:59:13 +0300

    Initial setup: Vue 3 + TS + Pinia + Naive UI

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import NaiveUI from 'naive-ui'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(NaiveUI)
+app.mount('#app')
